e0a1dc3b7135b7aeebd9b82783b2163e17f4a313
x34-dzt <shruvparmar@gmail.com>
2026-03-08 00:46:51 +0530

    feat: live long utils.rs

--- new/src/utils.rs
+++
@@ -1,8 +0,0 @@
-use ratatui::widgets::ListItem;
-
-pub fn construct_list_items<'a>(items: &'a [String]) -> Vec<ListItem<'a>> {
-    items
-        .iter()
-        .map(|item| ListItem::new(item.as_str()))
-        .collect()
-}
