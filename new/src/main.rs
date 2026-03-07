89d476eb5bf34f9982fcdadded1d99059f502b85
x34-dzt <shruvparmar@gmail.com>
2026-03-08 00:37:56 +0530

    chore: ignoring the wlx_action_handler for early stage development

--- new/src/main.rs
+++ new/src/main.rs
@@ -6,10 +6,11 @@ use crate::app::App;
 
 mod app;
 mod panes;
+mod utils;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let (wlx_emitter, wlx_events) = mpsc::sync_channel(16);
-    let (wlx_action_handler, wlx_action_rx) = mpsc::sync_channel(16);
+    let (_, wlx_action_rx) = mpsc::sync_channel(16);
     let (wlx_manager, wlx_eq) = WlMonitorManager::new_connection(wlx_emitter, wlx_action_rx)?;
 
     std::thread::spawn(move || -> Result<(), WlMonitorManagerError> {
